65160a4d96786a7aad742f33496e0141689e3a36
amaraSanoh <sanohawa@gmail.com>
2019-09-24 01:46:23 +0200

    interface de test. Fix : bug controller get.

--- src/main/java/fr/gpstationmeteo/repositories/MeteoRepository.java
+++ src/main/java/fr/gpstationmeteo/repositories/MeteoRepository.java
@@ -3,6 +3,7 @@ package fr.gpstationmeteo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fr.gpstationmeteo.entities.Meteo;
@@ -12,5 +13,9 @@ public interface MeteoRepository extends JpaRepository<Meteo, Long> {
 	
 	Meteo findById(int id);
 	List<Meteo> findByVilleNom(String nom);
+	
+	@Query(value = "SELECT * FROM meteo join ville on ville.nom = meteo.ville_nom WHERE meteo.jour = ?2 and ville.nom = ?1", 
+			  nativeQuery = true)
+	Meteo findMeteoByJour( String nom, String jour); 
 }
 
